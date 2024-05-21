1315f39ee8152d6598bc7df52059aa56db689edc
Галаев Максим <max@MacBook-Pro-Galaev.local>
2024-05-21 13:32:27 +0700

    Add ParseMarkdown function

--- pkg/handlers/render.go
+++ pkg/handlers/render.go
@@ -4,7 +4,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
+
+type BlogPost struct {
+	Title   string
+	Content string
+}
 
 func RenderTemplate(templatepath string, w http.ResponseWriter) error {
 	// parse html template
@@ -18,4 +25,16 @@ func RenderTemplate(templatepath string, w http.ResponseWriter) error {
 		log.Println(err.Error())
 	}
 	return err
+}
+
+func ParseMarkdown(MarkdownPath string) BlogPost {
+	markdownfile, err := os.ReadFile(MarkdownPath)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	title := string(markdownfile[0])
+	content := strings.Split(string(markdownfile), "___")
+	blogpost := BlogPost{Title: title, Content: content[1]}
+	return blogpost
+
 }
